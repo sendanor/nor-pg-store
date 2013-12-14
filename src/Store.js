64b1c338e542e5b7816e9a2dc09d8e5cadb7b090
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-12-14 21:05:18 +0200

    Updated

--- src/Store.js
+++ src/Store.js
@@ -5,121 +5,156 @@
 
 /* FIXME: update expiration so that the session does not expire when user is using the system! */
 
-module.exports = function(connect) {
-	var debug = require('nor-debug');
-	var util = require('util');
-	var pg = require('nor-pg');	
-	var Store = connect.session.Store;
+var debug = require('nor-debug');
+var util = require('util');
+var pg = require('nor-pg');	
+var Store = require('connect').session.Store;
 
-	function PgStore(options) {
-		options = options || {};
-		Store.call(this, options);
-		this._table = options.table || 'session';
-		this.config = options.pg;
+function PgStore(options) {
+	options = options || {};
+	//Store.call(this, options);
+	this._table = options.table || 'session';
+	this.config = options.pg;
+}
+
+PgStore.prototype = new Store();
+	
+/** Get session data */
+PgStore.prototype.get = function(sid, callback) {
+	var self = this;
+	var scope;
+
+	function do_success(db) {
+		var rows = db.fetch();
+		debug.log("PgStore.prototype.get(", sid, ") succeeds with rows: ", rows);
+		var data = rows.shift();
+		if(!(data && data.content)) {
+			//throw new TypeError('Failed to read session #' + sid);
+			callback(null);
+		} else {
+			callback(null, data.content);
+		}
 	}
+
+	function do_fail(err) {
+		debug.log("PgStore.prototype.get(", sid, ") failed: ", err);
+		scope.rollback(err);
+		callback(err);
+		//callback(null, {});
+	}
+
+	try {
+		var query = "SELECT content FROM "+'"' + self._table + '"'+" WHERE sid = $1";
+		debug.log("PgStore.prototype.get(", sid, "): query = " + query, " with sid=", sid);
+
+		scope = pg.scope();
+		pg.start(self.config).then(pg.scope(scope)).query(query, [sid]).then(do_success).commit().fail(do_fail).done();
+		//callback(null, {});
+	} catch(e) {
+		callback(e);
+	}
+};
 	
-	PgStore.prototype.__proto__ = Store.prototype;
-	
-	/** Get session data */
-	PgStore.prototype.get = function(sid, callback) {
-		function do_success(rows) {
-			debug.log("PgStore.prototype.get(", sid, ") succeeds with rows: ", rows);
-			var data = rows.shift().content;
-			if(!data) { throw new TypeError('Failed to read session #' + sid); }
-			callback(null, data);
-		}
+/** Set session data */
+PgStore.prototype.set = function(sid, session, callback) {
+	var scope;
 
-		function do_fail(err) {
-			debug.log("PgStore.prototype.get(", sid, ") failed: ", err);
-			scope.rollback(err);
-			callback(err);
-		}
+	function do_success() {
+		callback();
+	}
 
-		var self = this;
-		try {
-			var scope = pg.scope();
-			var query = "SELECT content FROM "+'"' + self._table + '"'+" WHERE sid = ?";
-			debug.log("PgStore.prototype.get(", sid, "): query = " + query, " with sid=", sid);
-			pg.start(self.config).then(pg.scope(scope)).query(query, [sid]).then(do_success).commit().fail(do_fail).done();
-		} catch(e) {
-			callback(e);
-		}
-	};
-	
-	/** Set session data */
-	PgStore.prototype.set = function(sid, session, callback) {
-		function do_success() {
-			callback();
-		}
+	function do_fail(err) {
+		debug.log("PgStore.prototype.set(", sid, ") failed: ", err);
+		scope.rollback(err);
+		callback(err);
+	}
 
-		function do_fail(err) {
-			scope.rollback(err);
-			callback(err);
-		}
+	var self = this;
+	try {
 
-		var self = this;
-		try {
-			var scope = pg.scope();
+		/*
+		var maxAge = session.cookie.maxAge,
+		    oneDay = 86400,
+			ttl = ('number' == typeof maxAge) ? (maxAge / 1000 | 0) : oneDay;
+		*/
 
-			/*
-			var maxAge = session.cookie.maxAge,
-			    oneDay = 86400,
-				ttl = ('number' == typeof maxAge) ? (maxAge / 1000 | 0) : oneDay;
-			*/
-
-			if(typeof callback !== 'function') {
-				callback = function(err) {
-					console.error('Error: ' + util.inspect(err) );
-				};
-			}
-
-			var query = "UPDATE "+'"' + self._table + '"'+" SET content = ? WHERE sid = ?";
-			debug.log("[PgStore.prototype.set] query = " + query);
-			pg.start(self.config).then(pg.scope(scope)).query(query, [session, sid]).then(do_success).commit().fail(do_fail).done();
-
-		} catch(e) {
-			if(callback) {
-				callback(e);
-			} else {
-				console.error('Error: ' + util.inspect(e) );
-			}
-		}
-	};
-	
-	/** Destroy session data */
-	PgStore.prototype.destroy = function(sid, callback) {
-		function do_success() {
-			callback();
-		}
-		
-		function do_fail(err) {
-			scope.rollback(err);
-			callback(err);
-		}
-
-		var self = this;
 		if(typeof callback !== 'function') {
 			callback = function(err) {
 				console.error('Error: ' + util.inspect(err) );
 			};
 		}
 
-		var query = "DELETE FROM "+'"' + self._table + '"'+" WHERE sid = ?";
-		debug.log("[PgStore.prototype.destroy] query = " + query);
-		pg.start(self.config).then(pg.scope(scope)).query(query, [sid]).then(do_success).commit().fail(do_fail).done();
-	};
-	
-	/*
-	PgStore.prototype.length = function(fn){
-		this.client.dbsize(fn);
-	};
+		debug.log("[PgStore.prototype.set] session = " + session);
+		debug.log("[PgStore.prototype.set] sid = " + sid);
 
-	PgStore.prototype.clear = function(fn){
-		this.client.flushdb(fn);
-	};
-	*/
-	
-	return PgStore;
+		var select_query = "SELECT COUNT(sid) AS count FROM "+'"' + self._table + '"'+" WHERE sid = $1";
+		debug.log("PgStore.prototype.set(", sid, "): select_query = " + select_query, " with sid=", sid);
+
+		var update_query = "UPDATE "+'"' + self._table + '"'+" SET content = $1 WHERE sid = $2";
+		debug.log("[PgStore.prototype.set(", sid, "] update_query = " + update_query);
+
+		var insert_query = "INSERT INTO "+'"' + self._table + '"'+" (content, sid) VALUES ($1, $2)";
+		debug.log("[PgStore.prototype.set(", sid, "] insert_query = " + insert_query);
+
+		scope = pg.scope();
+		pg.start(self.config).then(pg.scope(scope)).query(select_query, [sid]).then(function(db) {
+			var rows = db.fetch();
+			debug.log("[PgStore.prototype.set(", sid, "] rows = ", rows);
+			var count = parseInt(rows.shift().count, 10);
+			debug.log("[PgStore.prototype.set(", sid, "] count = ", count);
+			var exists = (count === 0) ? false : true;
+			debug.log("[PgStore.prototype.set(", sid, "] exists = ", exists);
+			var query = exists ? update_query : insert_query;
+			debug.log("[PgStore.prototype.set(", sid, "] query = ", query);
+			return db.query( query, [session, sid]);
+		}).commit().fail(do_fail).done();
+
+	} catch(e) {
+		if(callback) {
+			callback(e);
+		} else {
+			console.error('Error: ' + util.inspect(e) );
+		}
+	}
 };
+
+/** Destroy session data */
+PgStore.prototype.destroy = function(sid, callback) {
+	var scope;
+
+	function do_success() {
+		callback();
+	}
+		
+	function do_fail(err) {
+		debug.log("PgStore.prototype.destroy(", sid, ") failed: ", err);
+		scope.rollback(err);
+		callback(err);
+	}
+
+	var self = this;
+	if(typeof callback !== 'function') {
+		callback = function(err) {
+			console.error('Error: ' + util.inspect(err) );
+		};
+	}
+
+	var query = "DELETE FROM "+'"' + self._table + '"'+" WHERE sid = $1";
+	debug.log("[PgStore.prototype.destroy] query = " + query);
+	scope = pg.scope();
+	pg.start(self.config).then(pg.scope(scope)).query(query, [sid]).then(do_success).commit().fail(do_fail).done();
+};
+	
+/*
+PgStore.prototype.length = function(fn){
+	this.client.dbsize(fn);
+};
+
+PgStore.prototype.clear = function(fn){
+	this.client.flushdb(fn);
+};
+*/
+	
+module.exports = PgStore;
 
 /* EOF */
